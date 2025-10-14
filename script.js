a82d0b7407b1f744232cc01bbb5de4795eba60ba
Augusto ospino <augustoosrios@gmail.com>
2025-10-14 11:08:30 -0500

    manejo for js

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
-console.log("Hola mundo!!");
+/*let edad = -1;
 
-let a=9;
+if (edad >= 0) {
+  if (edad < 5) {
+    console.log("pRIMERA INFANCIA");
+  } else if (edad >= 5 && edad <= 12) {
+    console.log("INFANCIA ");
+  } else if (edad >= 12 && edad <= 18) {
+    console.log("ADOLECENCIA ");
+  } else {
+    console.log("ADULTEZ")
+  }
 
-/*Comentarios en js  */
+} else {
+  console.log("la edad no puede ser menor a cero!!!");
+}*/
+for (let c = 1; c <= 10; c++) {
+  
+  console.log("##################################");
+  console.log("Tabla del " + c );
+  
 
-if (a>=99){
-  console.log("es mayor!!");
+  for (let i = 1; i <= 10; i++) {
 
-}else{
-  console.log("es menor!!");  
+    console.log(c +" * " + i + " = " + i * c);
+
+  }
 }
